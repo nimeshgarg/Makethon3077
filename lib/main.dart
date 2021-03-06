ce1240da233daa24af0b6baa30b783005d3b9776
Nimesh Garg <nimeshgarg10@gmail.com>
2021-03-06 09:06:15 +0530

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-
+ 
 void main() {
   runApp(MaterialApp(
     home: MyApp(),
   ));
 }
-
+ 
 class MyApp extends StatefulWidget {
   @override
   _State createState() => _State();
 }
-
+ 
 class _State extends State<MyApp> {
   TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
-
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Login Page'),
+          title: Text('PWD Rojggar'),
         ),
         body: Padding(
             padding: EdgeInsets.all(10),
@@ -54,7 +53,18 @@ class _State extends State<MyApp> {
                   ),
                 ),
                 Container(
-                    height: 50,
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 50,
                     padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
                     child: RaisedButton(
                       textColor: Colors.white,
@@ -65,6 +75,7 @@ class _State extends State<MyApp> {
                         print(passwordController.text);
                       },
                     )),
+                
               ],
             )));
   }
